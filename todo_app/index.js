4a4b2c67df5121e8e9ed97eba883017dda695b9f
MYMISERABLEMIND17 <tomarparakram17@gmail.com>
2025-12-27 13:12:52 +0530

    task 4

--- todo_app/index.js
+++ todo_app/index.js
@@ -1,12 +1,27 @@
-const express = require("express");
-
+// app.js
+const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-    res.send("this is my first express file ");
-});
 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
+
+app.get('/' , function(req , res) {
+    res.send("hello")
+})
+
+app.use(express.json());
+app.post('/', (req, res)=>{
+    const {name} = req.body;
+    res.send(`Welcome ${name}`);
+})
+
+
+
+
+app.listen(PORT, (error) =>{
+    if(!error)
+        console.log("Server is Successfully Running, and App is listening on port "+ PORT);
+    else 
+        console.log("Error occurred, server can't start", error);
+    }
+);

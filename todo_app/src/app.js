1e93c0c25f5693e1d1f31f0623cd28d8ece2ee43
MYMISERABLEMIND17 <tomarparakram17@gmail.com>
2025-12-31 16:34:21 +0530

    file structure of backend

---
+++ todo_app/src/app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const todoRoutes = require('./routes/todo.routes');
+
+const app = express();
+
+app.use('/todos', todoRoutes);
+
+module.exports = app;
---
+++ todo_app/src/controller/todo.controller.js
@@ -0,0 +1,15 @@
+exports.todoHome = (req, res) => {
+    res.send('Todo home page');
+};
+
+exports.addTodo = (req, res) => {
+    res.send('Add todo page');
+};
+
+exports.editTodo = (req, res) => {
+    res.send('Edit todo page');
+};
+
+exports.deleteTodo = (req, res) => {
+    res.send('Delete todo page');
+};
